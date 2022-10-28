e7d7ef20fafc1f2cf74cf3925a717d44c73cfe83
ramyy220 <96258322+ramyy220@users.noreply.github.com>
2022-10-28 22:42:13 +0200

    Add files via upload

---
+++ exo03/exo3.java
@@ -0,0 +1,64 @@
+package exo3;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.awt.image.BufferedImage;
+import javax.imageio.*;
+import java.awt.Dimension;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+class  image {
+	 public int compareTo (Object other) {
+		 Object obj;
+		Object obj1;
+		if(other.equals(obj)==other.equals(obj1))
+			 return  0;
+		else if (other.equals(obj)<other.equals(obj1)) {
+			return -1;
+			}
+		else 
+			return 1;
+	        }
+static class FindMinMain{
+	void triat(int x,int y ) {
+	int c;
+	x=c;
+	x=y;
+	y=c;
+	  }
+
+	 public static void main(String[] args){  
+		 
+		 Image[] image=new Image[4];  
+		 Image("Scanner_thoracique_COVID19.png");
+		 Image("Pneumonie.png");
+		 Image("Cameraman128.png");
+		 Image("Baboongray.png ");
+		public void FindMin(int  tab[],int n) {
+	for(int i=0;i<tab.length-1;i++) {
+   
+    compareTo(tab[i],tab[i+1]);
+    }
+    FindMin(tab,n);
+    }
+	
+	private int compareTo(int i, int j) {
+			// TODO Auto-generated method stub
+			if(i>j) {
+			return 1;
+			}
+			else 
+				return 0;
+		}}
+
+	private static  void Image (String image){
+		// TODO Auto-generated method stub	
+		
+	}}
